222dd165bfbf63b590aca8fc849a81cfb15c69e0
de-odex <de-odex@users.noreply.github.com>
2022-11-02 22:36:31 +0900

    create continuous integration workflow

--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule MaiCafe.MixProject do
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -48,7 +55,9 @@ defmodule MaiCafe.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.15", only: :test, runtime: false}
     ]
   end
 
